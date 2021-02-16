98783eff9454cf7bf1a1394e51aae65fd4f06fc6
Katie Masiello <ka.masiello@gmail.com>
2021-02-15 22:44:45 -0800

    write scan_data

--- 01_adhoc_analysis/scan_data.R
+++ 01_adhoc_analysis/scan_data.R
@@ -1,1 +1,6 @@
-scan_data(strikes)
+library(pointblank)
+library(xml2)
+strikes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-23/wildlife_impacts.csv")
+
+scan <- scan_data(strikes)
+# write_html(scan, file = "scan_data.html")
